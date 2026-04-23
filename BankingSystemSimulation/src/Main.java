23a683b3a5ba0c49525c5513920a30da263a4897
Adithya Chukkala <161071658+chukkalaadithya@users.noreply.github.com>
2026-04-23 22:44:10 +0530

    Refactor FXMLLoader initialization in Main class

--- BankingSystemSimulation/src/Main.java
+++ BankingSystemSimulation/src/Main.java
@@ -7,20 +7,16 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-	    FXMLLoader loader = new FXMLLoader(
-	            getClass().getResource("/view/login.fxml")
-	    );
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
 
 	    Scene scene = new Scene(loader.load(), 1200, 700);
 
 	    stage.setScene(scene);
 	    stage.setTitle("Banking System");
 
-	    // Default window size
 	    stage.setWidth(1200);
 	    stage.setHeight(700);
 
-	    // Allow maximize
 	    stage.setMaximized(false);
 
 	    stage.show();
