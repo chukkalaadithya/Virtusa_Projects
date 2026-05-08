bb6245a97074bbe889b703e666172c5b87536b09
Adithya Chukkala <161071658+chukkalaadithya@users.noreply.github.com>
2026-05-09 01:04:26 +0530

    Update expense_manager.py

--- Expense Tracker Python Project/expense_manager.py
+++ Expense Tracker Python Project/expense_manager.py
@@ -4,13 +4,8 @@ import os
 FILE_NAME = "data.json"
 
 def load_data():
-    if not os.path.exists(FILE_NAME):
-        return []
-    try:
         with open(FILE_NAME, "r") as f:
             return json.load(f)
-    except:
-        return []
 
 def save_data(data):
     with open(FILE_NAME, "w") as f:
